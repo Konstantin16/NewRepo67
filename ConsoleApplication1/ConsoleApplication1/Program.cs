93f18d03ac9d5b675b3a4c9e63524dad7e409ce8
Усс.Константин <.@PC35-12-P>
2016-06-09 19:54:52 +0300

    уфмаыавмыа

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+   /*
+    Интерфейс Icomparable обеспечивает поведение объектов позволяющее выполнять их сравнение
+    Мы должны указать правила сравнения двух объектов, если объекты равны то необходимо вернуть "0",
+    "1" если наш объект больше предыдущего и "-1" если меньше
+    этот интерфейс используется для сортировки объектов
+         */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
